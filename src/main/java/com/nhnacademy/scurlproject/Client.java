2854218d460a8933adfd213cd1767a5ac8d76da4
liverbird <ehdgns07@gmail.com>
2022-04-20 09:17:39 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/scurlproject/Client.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.scurlproject;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Client {
+    public void ToDo1(String ToDo2) throws IOException {
+        Socket socket = new Socket(ToDo2,80);
+
+
+
+
+    }
+}
---
+++ src/main/java/com/nhnacademy/scurlproject/MainClass.java
@@ -0,0 +1,6 @@
+package com.nhnacademy.scurlproject;
+
+public class MainClass {
+    public static void main(String[] args) {
+    }
+}
